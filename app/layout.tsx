aa05d68aa33cf15fa43104e09c12cfe065b4ccb3
Ajo Koshy Joseph <ajokoshy@gmail.com>
2026-06-04 19:59:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'QuizCraft',
+  title: 'Quiz Craft By Ajo Koshy Joseph',
   description: 'Create and share beautiful quizzes',
 };
 
