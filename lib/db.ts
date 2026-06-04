4c9b9ef8b6e05d950104ecd1fa28c2094e2e8159
Ajo Koshy Joseph <ajokoshy@gmail.com>
2026-06-04 19:40:39 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL || 'postgresql://placeholder:placeholder@placeholder/placeholder');
 export default sql;
